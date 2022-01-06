8112eee04e0fc614b102e808ba2eb6a15e3c71d1
kaztro <00229017@uca.edu.sv>
2022-01-05 18:13:20 -0600

    Se eliminaron sonidos que ya no se utilizan

--- src/helpers/audio.js
+++ src/helpers/audio.js
@@ -10,11 +10,11 @@ Sound.setCategory('Ambient', true);
  
 /**
  * Variable que hace referencia al boton "Exhibiciones"
- */
+ 
 const buttonPress = new Sound(require('../audio/tuturu.mp3'), error => console.log(error));
 /**
  * Reproduce un sonido al presionar el boton de "Exhibiciones"
- */
+ 
 export const playButtonPress = () => {
   buttonPress.play();
 }
@@ -22,11 +22,11 @@ export const playButtonPress = () => {
 
 /**
  * Variable que hace referencia al boton "Recomendaciones Covid"
- */
+ 
 const heylisten = new Sound (require('../audio/heylisten.mp3'), error=> console.log(error));
 /**
  * Reproduce un sonido al presionar el boton de "Recomendaciones COVID"
- */
+ 
 export const heyListenButton = () =>{
   heylisten.play();
 }
@@ -46,11 +46,11 @@ export const wowButton = () =>{
 
 /**
  * Variable que hace referencia al boton "Sugerencias"
- */
+ 
 const msn = new Sound (require('../audio/msn.mp3'), error=> console.log(error));
 /**
  * Reproduce un sonido al presionar el boton de "Sugerencias"
- */
+ 
 export const msnButton = () =>{
   msn.play();
 }
@@ -58,11 +58,11 @@ export const msnButton = () =>{
 
 /**
  * Variable que hace referencia al boton "Preguntas Frecuentes"
- */
+ 
 const catc = new Sound (require('../audio/catch.mp3'), error=> console.log(error));
 /**
  * Reproduce un sonido al presionar el boton de "Preguntas Frecuentes"
- */
+ 
 export const catcButton = () =>{
   catc.play();
 }
@@ -70,11 +70,11 @@ export const catcButton = () =>{
 
 /**
  * Variable que hace referencia a los botones "Saber mas, conocela"
- */
+ 
 const coin = new Sound (require('../audio/coin.mp3'), error=> console.log(error));
 /**
  * Reproduce un sonido al presionar de los botones "Saber Mas", "Conocela", que se encuentran en cada una de las diferentes pantallas
- */
+ 
 export const coinButton = () =>{
   coin.play();
 }
@@ -82,14 +82,15 @@ export const coinButton = () =>{
 
 /**
  * Variable que hace referencia al boton "Conocenos mas"
- */
+ 
 const ohh = new Sound (require('../audio/ohh.mp3'), error=> console.log(error));
 /**
  * Reproduce un sonido al presionar de los botones "Conocenos Mas"
- */
+ 
 export const ohhButton = () =>{
   ohh.play();
 }
+*/
 
 const playQuiz = new Sound (require('../audio/quiz.mp3'), error => console.log(error));
 export const quizButton = () => {
